7eb97ba67f0867094b8b755880506dbc1e77b7c6
rakhiwho <rakhydubey04@gamil.com>
2025-01-02 17:36:13 +0530

    modified ts and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn", // Change 'error' to 'warn'
+      "@typescript-eslint/no-explicit-any": "off", // Disable the 'no-explicit-any' rule
+      "react-hooks/exhaustive-deps": "warn", // Change 'error' to 'warn'
+    },
+  },
 ];
 
 export default eslintConfig;
